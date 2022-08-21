7d428f0da1ebf1672fb73b720882a212e2ec00dc
Christian Tonye <christian.tonye@gmail.com>
2022-08-21 18:15:39 +0200

    chore(fix): fix message model
     - update generci class to unknow

--- src/core/models/messageModel.ts
+++ src/core/models/messageModel.ts
@@ -3,8 +3,8 @@ import { OnlineMessageType } from '../enums/onlineMessageType';
 import { SchedulerMessageType } from '../enums/schedulerMessageType';
 import { SchematicMessageType } from '../enums/schematicMessageType';
 
-export interface MessageModel<T> {
-  data: T;
+export interface MessageModel<unknow> {
+  data: unknow;
   type:
     | DatabaseMessageType
     | SchematicMessageType
