a4942398a1a6cb0398940c2cd805e40c15515c1e
Christian Tonye <christian.tonye@gmail.com>
2022-11-16 02:34:09 +0100

    fix(queType): fix name of queue
    - rename grioot to griiot

--- src/amqp/enums/queueType.ts
+++ src/amqp/enums/queueType.ts
@@ -1,7 +1,7 @@
 export enum QueueType {
   GRIIOT_BACKEND = 'griiot_schematic',
   GRIIOT_DATABASE = 'griiot_database',
-  GRIIOT_SCHEDULER = 'grioot_scheduler',
+  GRIIOT_SCHEDULER = 'griiot_scheduler',
   GRIIOT_ONLINE = 'griiot_online',
   GRIIOT_SCHEMATIC = 'griiot_schematic'
 }
