815efd26bae82f535b2ca53897340c721e57b41d
Christian <christian.tonye@gmail.com>
2023-09-05 00:44:04 +0200

    chore(dependencies):  new version
    - version + package.json

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -25,20 +25,18 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export function loggerInfo(id: string, path: string, data?: unknown): void {
-  logger.info(
-    `${getTitle(id, path)}${data ? `\ ${JSON.stringify(data)}` : ''}`
-  );
+  logger.info(`${getTitle(id, path)}${data ? ` ${JSON.stringify(data)}` : ''}`);
 }
 
 export function loggerDebug(id: string, path: string, data?: unknown): void {
   logger.debug(
-    `${getTitle(id, path)}${data ? `\ ${JSON.stringify(data)}` : ''}`
+    `${getTitle(id, path)}${data ? ` ${JSON.stringify(data)}` : ''}`
   );
 }
 
 export function loggerError(id: string, path: string, error: unknown): void {
   logger.error(
-    `${getTitle(id, path)}handled error... \n ${JSON.stringify(error)}`
+    `${getTitle(id, path)}handled error... ${JSON.stringify(error)}`
   );
 }
 
@@ -53,7 +51,7 @@ function getLogValue(
   data?: unknown
 ): string {
   return `[${domain}/${method}]${getKeyValue(id)}: ${
-    data ? `\ ${JSON.stringify(data)}` : ''
+    data ? `${JSON.stringify(data)}` : ''
   }`;
 }
 function getKeyValue(id?: { [key: string]: string | number }): string {
@@ -91,8 +89,47 @@ export class Logger {
         domain,
         method,
         id,
-        `handled error... \n ${JSON.stringify(error)}`
+        `handled error... ${JSON.stringify(error)}`
       )
     );
+  }
+}
+
+function getLogValue2(
+  domain: string,
+  method: string,
+  id?: string | number | null,
+  data?: unknown
+): string {
+  return `[${domain}][${method}]${id ? `[${id}]` : ''}: ${
+    data ? `${JSON.stringify(data)}` : ''
+  }`;
+}
+export class Logger2 {
+  static info(
+    domain: string,
+    method: string,
+    id?: string | number | null,
+    data?: unknown
+  ): void {
+    logger.info(getLogValue2(domain, method, id, data));
+  }
+
+  static debug(
+    domain: string,
+    method: string,
+    id?: string | number | null,
+    data?: unknown
+  ): void {
+    logger.debug(getLogValue2(domain, method, id, data));
+  }
+
+  static error(
+    domain: string,
+    method: string,
+    id: string | number | null,
+    error: unknown
+  ): void {
+    logger.error(getLogValue2(domain, method, id, `${JSON.stringify(error)}`));
   }
 }
