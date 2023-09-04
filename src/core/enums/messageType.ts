98ddd79ec6c0f7789294da055bfde584b75a6c14
Christian <christian.tonye@gmail.com>
2023-09-05 00:15:23 +0200

    chore(enum): Refactor enums
    - refactor Message type, infra status type, service status type

--- src/core/enums/messageType.ts
+++ src/core/enums/messageType.ts
@@ -3,10 +3,12 @@ import { DatabaseMessageType } from './databaseMessageType';
 import { OnlineMessageType } from './onlineMessageType';
 import { SchedulerMessageType } from './schedulerMessageType';
 import { SchematicMessageType } from './schematicMessageType';
+import { TechnicalMessageType } from './technicalMessageType';
 
 export type MessageType =
   | DatabaseMessageType
   | SchematicMessageType
   | SchedulerMessageType
   | OnlineMessageType
-  | ClientMessageType;
+  | ClientMessageType
+  | TechnicalMessageType;
--- src/core/enums/servicesStatusType.ts
+++ src/core/enums/servicesStatusType.ts
@@ -1,6 +1,5 @@
 export enum ServicesStatusType {
-  UP = 'UP',
-  DOWN = 'DOWN',
-  DELETED = 'DELETED',
-  FAILED_TO_DELETE = 'FAILED_TO_DELETE'
+  STOPPED = 'STOPPED',
+  RUNNING = 'RUNNING',
+  PENDING = 'PENDING'
 }
